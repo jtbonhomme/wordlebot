92e51625f7690bcbcd4a466ffd0ff27ad6911fbe
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2022-03-01 19:15:41 +0100

    parse word list

---
+++ cmd/first/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	var local = flag.String("l", "assets/words.txt", "use local words list")
+	var debug = flag.Bool("d", false, "display debug information")
+	flag.Parse()
+	log.Infoln("start with local words list ", *local)
+
+	if *debug {
+		log.SetLevel(log.DebugLevel)
+	}
+	file, err := os.Open(*local)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var words []string
+
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+	for _, word := range words {
+		log.Debugf("%s", word)
+	}
+}
